8002a2afa55d8c008196a0877d61788193666764
Marcin Grabysz <margrabysz@gmail.com>
2023-03-10 20:23:18 +0100

    Implement new method for moving

--- actions/go.pl
+++ actions/go.pl
@@ -1,3 +1,5 @@
+:- multifile look/0, i_am_at/1, at/2, path/2.
+
 /* These rules define the direction letters as calls to go/1. */
 
 n :- go(n).
@@ -11,6 +13,7 @@ w :- go(w).
 
 /* This rule tells how to move in a given direction. */
 
+% deprecated :)
 go(Direction) :-
         i_am_at(Here),
         path(Here, Direction, There),
@@ -20,3 +23,16 @@ go(Direction) :-
 
 go(_) :-
         write('You can''t go that way.').
+
+%  New method for moving
+go_to(Room) :-
+        i_am_at(Here),
+        path(Here, Room),
+        retract(i_am_at(Here)),
+        assert(i_am_at(Room)),
+        !, look.
+
+go_to(_) :-
+        write('There is not such place here').
+
+

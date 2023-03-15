786b89c4f46027c2efc142708b9c4bd946e7f0e5
sskarzyn <01158894@pw.edu.pl>
2023-03-15 20:57:31 +0100

    updated moving logic

--- actions/go.pl
+++ actions/go.pl
@@ -1,38 +1,13 @@
 :- multifile look/0, i_am_at/1, at/2, path/2.
 
-/* These rules define the direction letters as calls to go/1. */
+/* This rule tells how to move to given room. */
 
-n :- go(n).
-
-s :- go(s).
-
-e :- go(e).
-
-w :- go(w).
-
-
-/* This rule tells how to move in a given direction. */
-
-% deprecated :)
-go(Direction) :-
-        i_am_at(Here),
-        path(Here, Direction, There),
-        retract(i_am_at(Here)),
-        assert(i_am_at(There)),
-        !, look.
-
-go(_) :-
-        write('You can''t go that way.').
-
-%  New method for moving
-go_to(Room) :-
+go(Room) :-
         i_am_at(Here),
         path(Here, Room),
         retract(i_am_at(Here)),
         assert(i_am_at(Room)),
         !, look.
 
-go_to(_) :-
+go(_) :-
         write('There is not such place here').
-
-

25c5312daa6bf0df539c322fc3a0805abfd436d1
Szymon Skarzyński <szym.skarzynski@gmail.com>
2023-04-18 15:13:29 +0200

    fix dialogs

--- locations/elevator.pl
+++ locations/elevator.pl
@@ -3,11 +3,11 @@
 describe(elevator) :-
     write("You step in the elevator and when the doors are closing Michael (your boss) rushes into the elevator."), nl,
     write("Michael: Hi, you must be our new intern, welcome to Dunder Mifflin!"), nl,
-    write("(HINT: type \"hi.\" to say hi")"), nl,
+    write("(HINT: hi. to say hi)"), nl,
     read(_),
     write("Michael: During your internship you should obtain 3 (hopefully positive) reviews from other employees. Look around the office and see what you can do. When you are ready come to my room and I will take a look. Good luck!"), nl,
-    write("(HINT: type \"enter.\" to enter the office)").
-    
+    write("(HINT: enter. to enter the office)").
+
 enter :-
     nl,
     write("As you step out of the elevator you enter the office openspace..."), nl,

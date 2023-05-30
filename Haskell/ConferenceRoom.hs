c4aa6844721e75014a18e01e1719f4c41629b065
Szymon Skarzyński <rosomak909@gmail.com>
2023-05-30 21:23:38 +0200

    richer description

--- Haskell/ConferenceRoom.hs
+++ Haskell/ConferenceRoom.hs
@@ -3,4 +3,5 @@ module ConferenceRoom where
 describe :: IO ()
 describe = do
   putStrLn "You are in the confrence room"
-  putStrLn "You can go to the openspace from here"
+  putStrLn "There's nothing interesting here..."
+  putStrLn "You can go back to the openspace from here"
